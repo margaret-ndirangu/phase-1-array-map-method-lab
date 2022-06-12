54a9caeaac19b410deb8e71746d9bd18244aca1e
Margaret <wanjirundirangu43@gmail.com>
2022-06-12 23:17:05 +0300

    commit tree 99b2d9e16e66a84f0422265c75bff31cb96cee35

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = tutorials.map((tutorials) => {
+  return Object.assign({},tutorials, 
+  )})
